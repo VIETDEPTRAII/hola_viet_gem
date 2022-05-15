982558089d1fa7bd60ceb3d7595251e0cdb2f9ab
Nguyen Thanh Viet <nguyenthanhviet31031998@gmail.com>
2022-05-15 21:25:26 +0700

    add Rdoc to hola_viet.rb file

--- lib/hola_viet.rb
+++ lib/hola_viet.rb
@@ -1,4 +1,14 @@
+# The main HolaViet driver
 class HolaViet
+  # Say hi to the world!
+  #
+  # Example:
+  #   >> HolaViet.hi('spanish')
+  #   => Hola Vietdeptrai!
+  #
+  # Arguments:
+  #   language: (String)
+
   def self.hi(language = 'english')
     translator = Translator.new(language)
     translator.hi
